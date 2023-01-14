208fc8153cafaffa970e46b764be8df38149327e
ChetanXpro <chetanbaliyan10@gmail.com>
2023-01-15 00:23:59 +0530

    cors fix

--- config/corsOptons.js
+++ config/corsOptons.js
@@ -1,8 +1,7 @@
-
-
-const allowedOrigins = ["http://localhost:3000","http://localhost:5173","https://du-web-mp5m.vercel.app/","https://63c1b4bb30717f08a80bfd9c--transcendent-yeot-eeb75a.netlify.app"];
-
-
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://du-web-mp5m.vercel.app",
+];
 
 const corsOption = {
   origin: (origin, callback) => {
